177a2fafb989af7e2e2fb8a96955aa3416d1f611
YaroslavPikiner <yaroyay@gmail.com>
2019-08-22 16:25:36 +0300

    add firebase

---
+++ src/firebase.js
@@ -0,0 +1,28 @@
+import * as firebase from 'firebase';
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDf16W8zWHLVDcb_D_TMYkVxxEgnyuvtv4",
+    authDomain: "nasdasd-4830a.firebaseapp.com",
+    databaseURL: "https://nasdasd-4830a.firebaseio.com",
+    projectId: "nasdasd-4830a",
+    storageBucket: "",
+    messagingSenderId: "87349137076",
+    appId: "1:87349137076:web:7c93e4b39edc215d"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+
+
+  const firebaseDB = firebase.database()
+  const fireBaseArticles = firebaseDB.ref('articles');
+  const fireBaseTeams = firebaseDB.ref('teasm');
+  const fireBaseVideos = firebaseDB.ref('videos');
+
+  export {
+    firebase,
+    firebaseDB,
+    fireBaseArticles,
+    fireBaseTeams,
+    fireBaseVideos,
+  }
